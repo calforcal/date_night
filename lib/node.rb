989b3df374ed4595fa28c908dc680c274ba900a9
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2023-09-27 15:17:03 -0600

    Add left and right node to the node class

--- lib/node.rb
+++ lib/node.rb
@@ -1,8 +1,10 @@
 class Node
-  attr_reader :score, :title
-  
+  attr_reader :score, :title, :left_node, :right_node
+
   def initialize(score, title)
     @score = score
     @title = title
+    @left_node = nil
+    @right_node = nil
   end
 end
--- spec/binary_search_tree_spec.rb
+++ spec/binary_search_tree_spec.rb
@@ -5,8 +5,6 @@ RSpec.describe BinarySearchTree do
     it "exists" do
       tree = BinarySearchTree.new
       expect(tree).to be_a BinarySearchTree
-      expect(tree.left_node).to eq(nil)
-      expect(tree.right_node).to eq(nil)
     end
   end
 end
