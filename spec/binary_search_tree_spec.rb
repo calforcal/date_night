5209d9a51e9c1851381cfcb4b70c6b29edd39c38
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2023-09-27 15:20:33 -0600

    Add testing specs for BST insert method

--- spec/binary_search_tree_spec.rb
+++ spec/binary_search_tree_spec.rb
@@ -1,10 +1,19 @@
 require "spec_helper"
 
 RSpec.describe BinarySearchTree do
+  let!(:tree) { BinarySearchTree.new }
   describe "initialization" do
     it "exists" do
-      tree = BinarySearchTree.new
       expect(tree).to be_a BinarySearchTree
+    end
+  end
+
+  describe "#insert" do
+    it "can insert a node to the tree and return the tree level back" do
+      expect(tree.insert(61, "Bill & Ted's Excellent Adventure")).to eq(0)
+      expect(tree.insert(16, "Johnny English")).to eq(0)
+      expect(tree.insert(92, "Sharknado 3")).to eq(0)
+      expect(tree.insert(50, "Hannibal Buress: Animal Furnace")).to eq(0)
     end
   end
 end
