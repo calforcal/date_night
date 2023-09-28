c98d440fc8a0b95c8968f72011c486871715b8f0
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2023-09-28 08:25:19 -0600

    Adjust test specs

--- spec/binary_search_tree_spec.rb
+++ spec/binary_search_tree_spec.rb
@@ -11,9 +11,9 @@ RSpec.describe BinarySearchTree do
   describe "#insert" do
     it "can insert a node to the tree and return the tree level back" do
       expect(tree.insert(61, "Bill & Ted's Excellent Adventure")).to eq(0)
-      expect(tree.insert(16, "Johnny English")).to eq(0)
-      expect(tree.insert(92, "Sharknado 3")).to eq(0)
-      expect(tree.insert(50, "Hannibal Buress: Animal Furnace")).to eq(0)
+      expect(tree.insert(16, "Johnny English")).to eq(1)
+      expect(tree.insert(92, "Sharknado 3")).to eq(1)
+      expect(tree.insert(50, "Hannibal Buress: Animal Furnace")).to eq(2)
     end
   end
 end
